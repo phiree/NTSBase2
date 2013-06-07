78d0e69db7607840990c4c026db91da24b04de89
phiree <phiree@gmail.com>
2013-06-07 10:44:37 +0800

    导出功能

---
+++ Web/Admin/Products/ProductExport.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/admin.master" AutoEventWireup="true"
+    CodeFile="ProductExport.aspx.cs" Inherits="Admin_Products_ProductExport" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="pageDesc" runat="Server">
+    导出所有产品的英文资料
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <fieldset>
+        <legend>英文产品资料</legend>
+        <div>  导出<asp:TextBox runat="server" ID="tbxBeginDate"></asp:TextBox>之后的产品
+        
+            <uc:ButtonExt runat="server" ID="btnExportExcel" Text="导出Excel" OnClick="btnExportExcel_Click" />
+            <uc:ButtonExt runat="server" ID="btnExportImage" Text="导出图片" OnClick="btnExportImage_Click" />
+        </div>
+    </fieldset>
+
+     <fieldset>
+        <legend>没有图片的产品</legend>
+        <div>
+           <uc:ButtonExt runat="server" ID="btnExport_NoImage" Text="导出Excel" OnClick="btnExport_NoImage_Click" />
+          </div>
+    </fieldset>
+
+    <asp:Label runat="server" CssClass="info" ID="lblMsg"></asp:Label>
+</asp:Content>
---
+++ Web/Admin/Products/ProductExport.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NBiz;
+using NModel;
+using NLibrary;
+public partial class Admin_Products_ProductExport : System.Web.UI.Page
+{
+    ProductImagesExport imageExporter = new ProductImagesExport();
+    BizProduct bizProduct = new BizProduct();
+    IList<Product> productToExport;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    private IList<Product> ProductsWithEnglish
+    {
+        get {
+           
+                productToExport = bizProduct.GetProducts_English(Convert.ToDateTime(tbxBeginDate.Text ));
+           
+                return productToExport;
+        }
+    }
+    private IList<Product> ProductsNoImages
+    {
+        get
+        {
+            if (productToExport == null)
+            {
+                productToExport = bizProduct.GetProductsNoImages();
+            }
+            return productToExport;
+        }
+    }
+
+    protected void btnExportExcel_Click(object sender, EventArgs e)
+    {
+
+     
+    
+        ExcelExport export = new ExcelExport("产品资料" + DateTime.Now.ToString("yyyyMMdd-HHmmss"));
+        export.ExportProductExcel(ProductsWithEnglish);
+       
+        
+       
+
+
+       
+    }
+
+  
+    protected void btnExportImage_Click(object sender, EventArgs e)
+    {
+
+        NLogger.Logger.Debug("--开始导出图片--产品数量"+ProductsWithEnglish.Count);
+
+        imageExporter.Export(ProductsWithEnglish, Server.MapPath("/productImagesExport/") + DateTime.Now.ToString("yyyyMMdd-HHmmss") + "\\",
+
+      Server.MapPath("/ProductImages/original/"), NModel.Enums.ImageOutPutStratage.Category_NTsCode);
+
+        lblMsg.Text = "操作完成. 产品图片已保存于 \\192.168.1.44\\导出图片\\ ";
+        NLogger.Logger.Debug("--导出结束--");
+    }
+
+    protected void btnExport_NoImage_Click(object sender, EventArgs e)
+    {
+        ExcelExport export = new ExcelExport("没有图片的产品" + DateTime.Now.ToString("yyyyMMdd-HHmmss"));
+        export.ExportProductExcel(ProductsNoImages);
+        lblMsg.Text = "操作完成";
+       
+    }
+}
