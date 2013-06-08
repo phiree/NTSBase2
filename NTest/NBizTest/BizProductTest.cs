1fb9f8e67ba203fe7e8bb3b0e567b648a52a9c56
phiree <phiree@gmail.com>
2013-06-08 12:16:59 +0800

    单元测试- 增加dalmock, 删除不需要的文件

--- NTest/NBizTest/BizProductTest.cs
+++ NTest/NBizTest/BizProductTest.cs
@@ -5,15 +5,32 @@ using System.Text;
 using NModel;
 using NBiz;
 using NUnit.Framework;
+using Rhino.Mocks;
+using FizzWare.NBuilder;
 namespace NTest.NBizTest
 {
     [TestFixture]
     public class BizProductTest
     {
         BizProduct bizProduct = new BizProduct();
+        /// <summary>
+        /// 根据提供的供应商名称和型号,找出对应的产品
+        /// </summary>
         [Test]
         public void GetListByProvidedModelNumberSupplierNameListTest()
-        { 
+        {
+
+            var dalProduct = MockRepository.GenerateMock<NDAL.DALProduct>();
+            dalProduct.Expect(x => x.GetOneByModelNumberAndSupplierName("BP-811","百好","brighthome"))
+           .Return(Builder<Product>.CreateNew().Build());
+            var dalSupplier = MockRepository.GenerateMock<NDAL.DALSupplier>();
+            dalSupplier.Expect(x => x.GetOneByName("brighthome"))
+           .Return(Builder<Supplier>.CreateNew().With(x=>x.Name="百好").With(x=>x.EnglishName="brighthome"). Build());
+
+
+
+            bizProduct.DalProduct = dalProduct;
+            bizProduct.DalSupplier = dalSupplier;
             string list= @"brighthome---BP-811
 brighthome---BP-823
 brighthome---BP-852
@@ -23,6 +40,8 @@ brighthome---BP-BC32
             string msg;
             IList<Product> products = bizProduct.GetListByProvidedModelNumberSupplierNameList(list, out msg);
 
+
+            Assert.AreEqual(1, products.Count);
             
         }
     }
