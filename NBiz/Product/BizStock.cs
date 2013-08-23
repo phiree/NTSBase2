78a8f8d013e7eebf56826bf468e3cd180d7d7a6e
phiree <phiree@gmail.com>
2013-08-23 17:37:16 +0800

    dd

---
+++ NBiz/Product/BizStock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NModel;
+using NDAL;
+using NLibrary;
+using System.Data;
+using System.IO.Compression;
+namespace NBiz
+{
+    /// <summary>
+    /// 产品业务类.
+    /// </summary>
+    public class BizStock : BLLBase<NModel.ProductStock>
+    {
+      
+    }
+}
