d80460be21dc12c9c640dc655dbe33adfb7df19f
phiree <phiree@gmail.com>
2014-01-17 14:55:54 +0800

    ignore

---
+++ NDAL/DALSimpleMSSQl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using System.Data.SqlClient;
+namespace NDAL
+{
+    public class DALSimpleMSSQl
+    {
+        SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+
+        private void OpenConnection()
+        {
+            if (conn.State != System.Data.ConnectionState.Open)
+                conn.Open();
+        }
+        private void CloseConnection()
+        {
+            if (conn.State != System.Data.ConnectionState.Closed)
+                conn.Close();
+        }
+        public void ExcuteSql(string sql)
+        {
+            SqlCommand comm = new SqlCommand(sql, conn);
+            OpenConnection();
+            try
+            {
+                comm.ExecuteNonQuery();
+            }
+            catch
+            { 
+                throw; 
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+    }
+}
---
+++ Web/Admin/SyncCenter/Myanmar/Default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/empty.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Admin_SyncCenter_Myanmar_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+<span class="info">根据ERP系统单号,导出两个excel文件和一个图片文件夹</span>
+<span>输入ERP单据号,一行一条:</span><asp:TextBox TextMode=MultiLine runat="server" ID="tbxBillNoList"></asp:TextBox>
+<asp:Button runat="server" ID="btnExport" Text="导出" OnClick="btnExport_Click" />
+</asp:Content>
+
