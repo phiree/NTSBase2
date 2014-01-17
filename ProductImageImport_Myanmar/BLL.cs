9450740528a4fcd3d52d342d7ec8a3c9fc24177f
phiree <phiree@gmail.com>
2014-01-17 11:58:21 +0800

    Import product photo into myanmar's database

---
+++ ProductImageImport_Myanmar/BLL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+namespace ProductImageImport_Myanmar
+{
+    public class BLL
+    {
+        DAL dal = new DAL();
+        public void ReadImageDataToBytes()
+        { }
+        public void BuildBulkInsert(string foldPath)
+        {
+            string sql = string.Empty;
+            DirectoryInfo dir = new DirectoryInfo(foldPath);
+         //   FileInfo[] imageFiles = dir.GetFiles(foldPath);
+            string[] fileNames=Directory.GetFiles(foldPath);
+
+            dal.BulkUpdateImage(fileNames);
+        }
+    }
+}
